2f60e2d1a0aecbfc822ff111bde4de69c0909f21
Johnny Anderson dos Santos <johnnyadsantos@gmail.com>
2018-02-15 15:45:42 -0200

    Add autofac extension

--- extensions/SimpleHttpFactory.Extensions.AutoFac/AutoFacContainerBuilderExtensions.cs
+++ extensions/SimpleHttpFactory.Extensions.AutoFac/AutoFacContainerBuilderExtensions.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using Autofac;
+using Autofac.Builder;
 
 namespace SimpleHttpFactory.Extensions.AutoFac
 {
     public static class AutoFacContainerBuilderExtensions
     {
+        public static IRegistrationBuilder<SimpleHttpFactory, SimpleActivatorData, SingleRegistrationStyle>
+            RegisterSimpleHttpFactory(
+                this ContainerBuilder cb
+                , Action<SimpleHttpFactory> configure
+            )
+        {
+            return cb.Register(ctx =>
+                     {
+                         var factory = new SimpleHttpFactory();
+
+                         configure(factory);
+
+                         return factory;
+                     })
+                     .As<ISimpleHttpFactory>();
+        }
     }
 }
