703d05d6763ce00297eec6da899443ec2c4539f6
Johnny Anderson dos Santos <johnnyadsantos@gmail.com>
2018-02-15 16:28:49 -0200

    Add more tests

--- tests/SimpleHttpFactory.Tests/SimpleHttpFactoryTests.cs
+++ tests/SimpleHttpFactory.Tests/SimpleHttpFactoryTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SimpleHttpFactory.Tests
@@ -8,11 +11,11 @@ namespace SimpleHttpFactory.Tests
         [Fact]
         public void SimpleUsageTest()
         {
-            var sut = new SimpleHttpFactory();
+            var simpleHttpFactory = new SimpleHttpFactory();
             var baseAddress = new Uri("http://0.0.0.0:9090");
             var timeout = TimeSpan.FromMinutes(5);
 
-            sut.AddClientFactory(
+            simpleHttpFactory.AddClientFactory(
                 "test",
                 opt => opt.ClientConfiguration = c =>
                 {
@@ -20,10 +23,47 @@ namespace SimpleHttpFactory.Tests
                     c.Timeout = timeout;
                 }
             );
-            var client = sut.CreateClient("test");
+            var client = simpleHttpFactory.CreateClient("test");
 
             Assert.Same(baseAddress, client.BaseAddress);
             Assert.Equal(timeout, client.Timeout);
+        }
+
+        [Fact]
+        public async Task ShouldBuildAHttpClientWithMessageHandlerIfAny()
+        {
+            var simpleHttpFactory = new SimpleHttpFactory();
+
+            simpleHttpFactory.AddClientFactory(
+                "test",
+                opt => opt.MessageHandler = new TestMessageHandler()
+            );
+            var client = simpleHttpFactory.CreateClient("test");
+
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(() => client.GetAsync("http://0.0.0.0"));
+            Assert.Equal("yeah it is right", exception.Message);
+        }
+
+        [Fact]
+        public void ShouldBuildEvenWhenThereIsNoOptionsSet()
+        {
+            var simpleHttpFactory = new SimpleHttpFactory();
+
+            simpleHttpFactory.AddClientFactory("test", _ => {});
+            var client = simpleHttpFactory.CreateClient("test");
+
+            Assert.NotNull(client);
+        }
+    }
+
+    public class TestMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request
+            , CancellationToken cancellationToken
+        )
+        {
+            throw new NotImplementedException("yeah it is right");
         }
     }
 }
