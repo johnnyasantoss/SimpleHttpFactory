3ec9e140ea2c639a36bef993fc21f11c66165eda
Johnny Anderson dos Santos <johnnyadsantos@gmail.com>
2018-02-22 18:12:19 -0300

    Add structure map extension

---
+++ extensions/SimpleHttpFactory.Extensions.StructureMap/StructureMapContainerBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using StructureMap;
+
+namespace SimpleHttpFactory.Extensions.StructureMap
+{
+    public static class StructureMapContainerBuilderExtensions
+    {
+        public static IContainer ConfigureSimpleHttpFactory(
+            this IContainer container
+            , Action<SimpleHttpFactory> configure
+        )
+        {
+            container.Configure(c => c.ConfigureSimpleHttpFactory(configure));
+
+            return container;
+        }
+
+        public static ConfigurationExpression ConfigureSimpleHttpFactory(
+            this ConfigurationExpression c
+            , Action<SimpleHttpFactory> configure
+        )
+        {
+            var httpFac = new SimpleHttpFactory();
+
+            configure?.Invoke(httpFac);
+
+            c.ForSingletonOf<ISimpleHttpFactory>()
+             .Use(httpFac);
+
+            return c;
+        }
+    }
+}
---
+++ tests/SimpleHttpFactory.Extensions.StructureMap.Tests/StructureMapTests.cs
@@ -0,0 +1,33 @@
+using StructureMap;
+using Xunit;
+
+namespace SimpleHttpFactory.Extensions.StructureMap.Tests
+{
+    public class StructureMapTests
+    {
+        [Fact]
+        public void ContainerExtensionTests()
+        {
+            var container = new Container()
+                .ConfigureSimpleHttpFactory(f => f.AddClientFactory("test", _ => {}));
+
+            var factory = container.GetInstance<ISimpleHttpFactory>();
+            Assert.NotNull(factory);
+
+            var testClient = factory.CreateClient("test");
+            Assert.NotNull(testClient);
+        }
+        
+        [Fact]
+        public void ContainerExpressionsExtensionTests()
+        {
+            var container = new Container(c => c.ConfigureSimpleHttpFactory(f => f.AddClientFactory("test", _ => {})));
+
+            var factory = container.GetInstance<ISimpleHttpFactory>();
+            Assert.NotNull(factory);
+
+            var testClient = factory.CreateClient("test");
+            Assert.NotNull(testClient);
+        }
+    }
+}
